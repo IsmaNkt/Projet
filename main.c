7e8c4d666d3c800ddc59d97c75d0933255c9fa71
Hugotpx <troupeaudebananes0@gmail.com>
2024-05-26 00:01:25 +0200

    Update main.c

--- main.c
+++ main.c
@@ -38,6 +38,7 @@ int main() {
     switch (choix) {
       case 1:
         creerFichier(nbr_athletes, nom_athletes);
+        nbr_athletes = 0; // remet à 0 le nbr d'athlètes
         break;
 
       case 2: 
