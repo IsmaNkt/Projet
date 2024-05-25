b66d6312da9ac0d64788a49c6d6c08ffd6d7fb62
Hugotpx <troupeaudebananes0@gmail.com>
2024-05-26 00:29:33 +0200

    Update menu.c

--- menu.c
+++ menu.c
@@ -7,21 +7,19 @@ void afficher_menu() {
   printf("=======================================================\n");
   printf("                 GESTION DES ATHLÈTES         \n");
   printf("=======================================================\n");
-  printf("1. Créer un fichier pour un athlète\n");
+  printf("1. Ajouter un entraînement pour un athlète\n");
   printf("-------------------------------------------------------\n");
-  printf("2. Ajouter un entraînement pour un athlète\n");
+  printf("2. Historique des entrainements : Par athlète / épreuve / date.\n");
   printf("-------------------------------------------------------\n");
-  printf("3. Historique des entrainements : Par athlète / épreuve / date.\n");
+  printf("3. Afficher les performances d'un athlète\n");
   printf("-------------------------------------------------------\n");
-  printf("4. Afficher les performances d'un athlète\n");
+  printf("4. 3 meilleurs athlètes pour une discipline\n");
   printf("-------------------------------------------------------\n");
-  printf("5. 3 meilleurs athlètes pour une discipline\n");
+  printf("5. Différence de temps\n");
   printf("-------------------------------------------------------\n");
-  printf("6. Différence de temps\n");
+  printf("6. 3 pires athlètes pour une discipline\n");
   printf("-------------------------------------------------------\n");
-  printf("7. 3 pires athlètes pour une discipline\n");
-  printf("-------------------------------------------------------\n");
-  printf("8. Quitter\n");
+  printf("7. Quitter\n");
   printf("=======================================================\n");
   printf("Choisissez une option (numéro): ");
 }
